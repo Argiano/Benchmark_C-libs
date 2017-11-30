d18fbc1042db69214ce99abd1576dfac99b0b3f6
WallKroftPanda <rorro2997@gmail.com>
2017-11-29 23:20:35 -0300

    Update vectorex.cpp

--- vectorex.cpp
+++ vectorex.cpp
@@ -4,6 +4,7 @@
 #include <cstdio>
 #include <cstdlib>
 #include <iostream>
+#include <omp.h>
 
 using namespace std;
 
@@ -13,6 +14,7 @@ int main(){
     //const int base = stoi(argv[1]);
     int l=1000,base=1000;
     int size = base+20;
+    double tstart,tend,timeIns=0,timeSearch=0,timeElim=0;
     vector<int>myvector;
 
 
@@ -24,11 +26,15 @@ int main(){
             myvector.push_back(ins);
         }   
         //insertar
+        tstart = omp_get_wtime();
         for(i=0; i<20; i++){
             ins=rand()%1000;
             myvector.push_back(ins);
         }
+        tend = omp_get_wtime();
+        timeIns += (tend-tstart); 
         //buscar
+        tstart = omp_get_wtime();
         for(i=0; i<20; i++){
             j=0;
             ins=rand()%1000;
@@ -39,11 +45,19 @@ int main(){
                 it++;
             }
         }
+        tend = omp_get_wtime();
+        timeSearch += (tend-tstart);
         //eliminar
+        tstart = omp_get_wtime();
         for(i=0;i<20;i++){
             myvector.pop_back();
         }
+        tend = omp_get_wtime();
+        timeElim += (tend-tstart);
         l+=1000;
     }
+    cout<<"time ins: "<<timeIns/1000000<<endl;
+    cout<<"time search: "<<timeSearch/1000000<<endl;
+    cout<<"time Elim: "<<timeElim/1000000<<endl;
     return 0;
 }
